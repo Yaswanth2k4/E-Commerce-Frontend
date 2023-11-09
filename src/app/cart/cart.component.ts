090a80276fc5345519074504964443f2338af614
Yaswanth2k4 <kurapati.yeswanth64@gmail.com>
2023-11-09 18:45:29 +0530

    added badge to cart icon

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -9,19 +9,6 @@ import { CartService } from '../cart.service';
 export class CartComponent {
 
   constructor(public cs:CartService){}
-  
-  imagePath: string="assets/images/anthurium.png";
-  orig_price: string="1014";
-  price:string="559 (45% off)";
-  title="Lushy Spider plant(Chlorophytum)";
-  review="277";
-  discount="Extra 30% Instant Discount";
-  style={
-    "width":"80%",
-    "height":"auto",
-    "margin-left":"20px",
-    "margin-top":"10px"
-  }
 
   close(event:MouseEvent)
   {
